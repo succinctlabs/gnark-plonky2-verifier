c4b53e8c47852665c912bccf0ff4bc54676e597e
Kevin Jue <kjue235@gmail.com>
2023-06-15 11:33:22 -0700

    used gnark cherry picked bug fix

--- verifier/verifier_test.go
+++ verifier/verifier_test.go
@@ -3,7 +3,9 @@ package verifier_test
 import (
 	"testing"
 
+	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/std/math/emulated"
 	"github.com/consensys/gnark/test"
 	"github.com/succinctlabs/gnark-plonky2-verifier/field"
 	"github.com/succinctlabs/gnark-plonky2-verifier/verifier"
@@ -54,4 +56,53 @@ func TestStepVerifier(t *testing.T) {
 		assert.NoError(err)
 	}
 	testCase()
+}
+
+func TestStepVerifier2(t *testing.T) {
+	assert := test.NewAssert(t)
+
+	plonky2Circuit := "step"
+	proofWithPis := utils.DeserializeProofWithPublicInputs("./data/" + plonky2Circuit + "/proof_with_public_inputs.json")
+	circuit := TestVerifierCircuit{
+		plonky2CircuitName: plonky2Circuit,
+		Proof:              proofWithPis.Proof,
+		PublicInputs:       proofWithPis.PublicInputs,
+	}
+
+	proofWithPis2 := utils.DeserializeProofWithPublicInputs("./data/" + plonky2Circuit + "/proof_with_public_inputs.json")
+	witness := TestVerifierCircuit{
+		plonky2CircuitName: plonky2Circuit,
+		Proof:              proofWithPis2.Proof,
+		PublicInputs:       proofWithPis2.PublicInputs,
+	}
+
+	assert.ProverSucceeded(&circuit, &witness, test.WithCurves(ecc.BN254))
+}
+
+type testCircuit struct {
+	Arr      [2]emulated.Element[emulated.Secp256k1Fp]
+	Expected emulated.Element[emulated.Secp256k1Fp]
+}
+
+func (circuit *testCircuit) Define(api frontend.API) error {
+	field, _ := emulated.NewField[emulated.Secp256k1Fp](api)
+
+	mulRes := field.Mul(&circuit.Arr[0], &circuit.Arr[1])
+	field.AssertIsEqual(mulRes, &circuit.Expected)
+
+	return nil
+}
+
+func TestMain(t *testing.T) {
+	assert := test.NewAssert(t)
+
+	var circuit testCircuit
+
+	assert.ProverSucceeded(&circuit, &testCircuit{
+		Arr: [2]emulated.Element[emulated.Secp256k1Fp]{
+			emulated.ValueOf[emulated.Secp256k1Fp](42),
+			emulated.ValueOf[emulated.Secp256k1Fp](24),
+		},
+		Expected: emulated.ValueOf[emulated.Secp256k1Fp](1008),
+	}, test.WithCurves(ecc.BN254))
 }
