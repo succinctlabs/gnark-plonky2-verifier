6e87ac9aa6e42de5737eb7a4edb3b5d0726b4cf7
Kevin Jue <kjue235@gmail.com>
2023-06-27 17:14:50 -0700

    fixed proof_server.go

--- proof_server.go
+++ proof_server.go
@@ -2,34 +2,148 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"math/big"
 	"net"
 	"os"
+	"time"
 
+	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/backend/groth16"
 	"github.com/consensys/gnark/constraint"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/r1cs"
+	"github.com/consensys/gnark/profile"
+	"github.com/succinctlabs/gnark-plonky2-verifier/field"
+	"github.com/succinctlabs/gnark-plonky2-verifier/verifier"
+	"github.com/succinctlabs/gnark-plonky2-verifier/verifier/common"
+	"github.com/succinctlabs/gnark-plonky2-verifier/verifier/utils"
 )
 
-const SockAddr = "/tmp/echo.sock"
+type Plonky2VerifierCircuit struct {
+	Proof        common.Proof
+	PublicInputs []field.F `gnark:",public"`
 
-func generateProof(conn net.Conn, r1cs constraint.ConstraintSystem, pk groth16.ProvingKey, vk groth16.VerifyingKey) {
-	log.Printf("Client connected [%s]", conn.RemoteAddr().Network())
-	defer conn.Close()
+	verifierChip       *verifier.VerifierChip `gnark:"-"`
+	plonky2CircuitName string                 `gnark:"-"`
+}
 
-	buf := []byte{}
-	_, err := conn.Read(buf)
+func (circuit *Plonky2VerifierCircuit) Define(api frontend.API) error {
+	circuitDirname := "./verifier/data/" + circuit.plonky2CircuitName + "/"
+	commonCircuitData := utils.DeserializeCommonCircuitData(circuitDirname + "common_circuit_data.json")
+	verifierOnlyCircuitData := utils.DeserializeVerifierOnlyCircuitData(circuitDirname + "verifier_only_circuit_data.json")
+
+	circuit.verifierChip = verifier.NewVerifierChip(api, commonCircuitData)
+
+	circuit.verifierChip.Verify(circuit.Proof, circuit.PublicInputs, verifierOnlyCircuitData, commonCircuitData)
+
+	return nil
+}
+
+func compileCircuit(plonky2Circuit string, profileCircuit bool, serialize bool, outputSolidity bool) (constraint.ConstraintSystem, groth16.ProvingKey, groth16.VerifyingKey) {
+	circuit := Plonky2VerifierCircuit{
+		plonky2CircuitName: plonky2Circuit,
+	}
+	proofWithPis := utils.DeserializeProofWithPublicInputsFromFile("./verifier/data/" + plonky2Circuit + "/proof_with_public_inputs.json")
+	circuit.Proof = proofWithPis.Proof
+	circuit.PublicInputs = proofWithPis.PublicInputs
+
+	var p *profile.Profile
+	if profileCircuit {
+		p = profile.Start()
+	}
+	r1cs, err := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &circuit)
 	if err != nil {
-		log.Printf("Error reading from socket: %s", err)
-		return
+		fmt.Println("error in building circuit", err)
+		os.Exit(1)
 	}
 
-	proof := createProof(string(buf), r1cs, pk, vk, false)
+	if profileCircuit {
+		p.Stop()
+		p.Top()
+		println("r1cs.GetNbCoefficients(): ", r1cs.GetNbCoefficients())
+		println("r1cs.GetNbConstraints(): ", r1cs.GetNbConstraints())
+		println("r1cs.GetNbSecretVariables(): ", r1cs.GetNbSecretVariables())
+		println("r1cs.GetNbPublicVariables(): ", r1cs.GetNbPublicVariables())
+		println("r1cs.GetNbInternalVariables(): ", r1cs.GetNbInternalVariables())
+	}
+
+	// Don't serialize the circuit for now, since it takes up too much memory
+	/*
+		if serialize {
+			fR1CS, _ := os.Create("circuit")
+			r1cs.WriteTo(fR1CS)
+			fR1CS.Close()
+		}
+	*/
+
+	fmt.Println("Running circuit setup", time.Now())
+	pk, vk, err := groth16.Setup(r1cs)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	if serialize {
+		fPK, _ := os.Create("proving.key")
+		pk.WriteTo(fPK)
+		fPK.Close()
+
+		fVK, _ := os.Create("verifying.key")
+		vk.WriteTo(fVK)
+		fVK.Close()
+	}
+
+	if outputSolidity {
+		fSolidity, _ := os.Create("proof.sol")
+		err = vk.ExportSolidity(fSolidity)
+	}
+
+	return r1cs, pk, vk
+}
+
+func createProof(serializedProofWithPI string, r1cs constraint.ConstraintSystem, pk groth16.ProvingKey, vk groth16.VerifyingKey, serialize bool) groth16.Proof {
+	proofWithPis := utils.DeserializeProofWithPublicInputs(serializedProofWithPI)
+
+	// Witness
+	assignment := &Plonky2VerifierCircuit{
+		Proof:        proofWithPis.Proof,
+		PublicInputs: proofWithPis.PublicInputs,
+	}
+
+	fmt.Println("Generating witness", time.Now())
+	witness, _ := frontend.NewWitness(assignment, ecc.BN254.ScalarField())
+	publicWitness, _ := witness.Public()
+	if serialize {
+		fWitness, _ := os.Create("witness")
+		witness.WriteTo(fWitness)
+		fWitness.Close()
+	}
+
+	fmt.Println("Creating proof", time.Now())
+	proof, err := groth16.Prove(r1cs, pk, witness)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if serialize {
+		fProof, _ := os.Create("proof.proof")
+		proof.WriteTo(fProof)
+		fProof.Close()
+	}
+
+	fmt.Println("Verifying proof", time.Now())
+	err = groth16.Verify(proof, vk, publicWitness)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	const fpSize = 4 * 8
-	var proofBuf bytes.Buffer
-	proof.WriteRawTo(&proofBuf)
-	proofBytes := proofBuf.Bytes()
+	var buf bytes.Buffer
+	proof.WriteRawTo(&buf)
+	proofBytes := buf.Bytes()
 
 	var (
 		a [2]*big.Int
@@ -57,9 +171,27 @@ func generateProof(conn net.Conn, r1cs constraint.ConstraintSystem, pk groth16.P
 
 	println("c[0] is ", c[0].String())
 	println("c[1] is ", c[1].String())
+
+	return proof
+}
+
+func generateProof(conn net.Conn, r1cs constraint.ConstraintSystem, pk groth16.ProvingKey, vk groth16.VerifyingKey) {
+	log.Printf("Client connected [%s]", conn.RemoteAddr().Network())
+	defer conn.Close()
+
+	buf := []byte{}
+	_, err := conn.Read(buf)
+	if err != nil {
+		log.Printf("Error reading from socket: %s", err)
+		return
+	}
+
+	createProof(string(buf), r1cs, pk, vk, false)
 }
 
 func main() {
+	const SockAddr = "/tmp/echo.sock"
+
 	r1cs, pk, vk := compileCircuit("step", false, false, false)
 
 	if err := os.RemoveAll(SockAddr); err != nil {
