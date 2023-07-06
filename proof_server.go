19163ec355c1827020dc917eab764e6054cf23ec
Kevin Jue <kjue235@gmail.com>
2023-07-06 14:52:07 -0700

    removed prints and output the solidity file

--- proof_server.go
+++ proof_server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"encoding/hex"
 	"fmt"
 	"log"
 	"math/big"
@@ -187,11 +186,8 @@ func generateProof(conn net.Conn, r1cs constraint.ConstraintSystem, pk groth16.P
 		buf := make([]byte, 8192)
 		n, err := conn.Read(buf)
 		totalRead += n
-		println("read from socket.  totalRead is ", totalRead, "err is ", err)
-
 		serializedProof = append(serializedProof, buf[:n]...)
 
-		println("last byte is ", hex.EncodeToString(serializedProof[len(serializedProof)-1:len(serializedProof)]))
 		if serializedProof[len(serializedProof)-1] == 0x1e {
 			serializedProof = serializedProof[:len(serializedProof)-1]
 			break
@@ -219,7 +215,7 @@ func generateProof(conn net.Conn, r1cs constraint.ConstraintSystem, pk groth16.P
 func main() {
 	const SockAddr = "/tmp/echo.sock"
 
-	r1cs, pk, vk := compileCircuit("step", false, false, false)
+	r1cs, pk, vk := compileCircuit("step", false, false, true)
 	println("Done compiling circuit")
 
 	if err := os.RemoveAll(SockAddr); err != nil {
