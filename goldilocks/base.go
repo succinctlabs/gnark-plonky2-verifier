2ed3572c012fa30c3b32d589ff64c6e9a52d5cbd
John Guibas <john@succinct.xyz>
2023-07-11 10:07:55 -0700

    Update goldilocks/base.go
    Co-authored-by: Kevin Jue <kjue235@gmail.com>

--- goldilocks/base.go
+++ goldilocks/base.go
@@ -170,7 +170,7 @@ func (p *Chip) Reduce(x Variable) Variable {
 	// 		MODULUS * quotient + remainder = x
 	//
 	// Must check that remainder \in [0, MODULUS) and quotient \in [0, 2^RANGE_CHECK_NB_BITS) to ensure
-	// that this computation does not overflow. We use 2^130 to reduce the cost of the range check
+	// that this computation does not overflow. We use 2^140 to reduce the cost of the range check
 	// and because 2^RANGE_CHECK_NB_BITS * 2^64 = 2^194 < p < 2^254.
 
 	result, err := p.api.Compiler().NewHint(ReduceHint, 2, x.Limb)
