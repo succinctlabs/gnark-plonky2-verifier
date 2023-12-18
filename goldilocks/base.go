85d20ce6569aaa4c71454f7976c2a5a724dcd159
Kevin Jue <kjue235@gmail.com>
2023-12-18 12:46:53 -0800

    fix for V-SCT-VUL-002

--- goldilocks/base.go
+++ goldilocks/base.go
@@ -190,6 +190,9 @@ func (p *Chip) Reduce(x Variable) Variable {
 
 	remainder := NewVariable(result[1])
 	p.RangeCheck(remainder)
+
+	p.api.AssertIsEqual(x, p.api.Add(p.api.Mul(quotient, MODULUS), remainder.Limb))
+
 	return remainder
 }
 
